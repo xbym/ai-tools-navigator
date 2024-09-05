07e6b27c8456b69ccf8edc19acf54001c34ca61b
xbym <78540474+xbym@users.noreply.github.com>
2024-09-05 22:57:54 +0900

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... other configs
   env: {
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  // Sentry options
-  // ...
-});
+export default nextConfig;
