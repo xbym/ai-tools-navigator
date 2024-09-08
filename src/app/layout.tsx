1b240039ebac915369d631f3bdfe65bd12c6d13a
xbym <78540474+xbym@users.noreply.github.com>
2024-09-08 19:28:10 +0900

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ToastProvider } from '@/contexts/ToastContext';
 import "./globals.css";
 import ErrorBoundary from '@/components/ErrorBoundary';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
@@ -23,6 +24,7 @@ export default function RootLayout({
           </AuthProvider>
         </ErrorBoundary>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
